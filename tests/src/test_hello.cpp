d6ce05b1b8bd84da50eee747d752b3196e748792
learnforpractice <learnforpractice@gmail.com>
2022-10-22 19:27:33 +0800

    Update tests

--- tests/src/test_hello.cpp
+++ tests/src/test_hello.cpp
@@ -5,31 +5,19 @@
 
 TEST_CASE( "test hello", "[hello]" ) {
     // load_native_contract(HELLO_SO);
-    set_apply(hello_native_apply);
+    set_native_apply(hello_native_apply);
 
     ChainTester tester(true);
-    tester.enable_debug_contract("hello", true);
+    tester.enable_debug_contract("hello"_n, true);
 
-    tester.deploy_contract("hello", HELLO_WASM, HELLO_ABI);
+    tester.deploy_contract("hello"_n, HELLO_WASM, HELLO_ABI);
 
-    auto permissions = R""""(
-    {
-        "hello": "active"
-    }
-    )"""";
-    auto args = R""""(
-    {
-        "a": "hello",
-        "b": "alice"
-    }
-    )"""";
-    tester.push_action("hello", "test", args, permissions);
+    vector<permission_level> permissions = {{"hello"_n, "active"_n}};
+
+    auto args = std::make_tuple("hello"_n, "alice"_n);
+    tester.push_action("hello"_n, "test"_n, args, permissions);
     tester.produce_block();
-{
-    //action arguments in raw data format
-    auto args = eosio::pack(std::make_tuple(eosio::name("hello"), eosio::name("alice")));
-    tester.push_action("hello", "test", args, permissions);
-    tester.produce_block();
-}
 
+    tester.push_action("hello"_n, "test"_n, args, "hello"_n);
+    tester.produce_block();
 }
